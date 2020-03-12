4f98b1bc4271038fd4a568662e70be11c0e8e6d4
Oleg <jekkicool@yandex.ru>
2020-03-12 11:10:12 +0300

    add helpers

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const { getItilsolutions } = require("./db");
-const workRegexp = /\d{2}\.\d{2}\.\d{4}/g;
+const WorksCollection = require("./components/works");
 
 async function main() {
     const itilsolutions = await getItilsolutions();
-    console.log(itilsolutions);
+    const ticketsInfo = itilsolutions.map(createTicketInfo);
+    const totalSumm = calculateTotalSumm(ticketsInfo);
 }
 
-main()
-    .catch(console.error);
+// парсим список работ по заявке
+function parseTicketWorks(content) {
+    const workRegexp = /\d{2}\.\d{2}\.\d{4}/g;
+    const works = content.match(workRegexp);
+    return works || [];
+}
+
+// считаем общую цену по заявке
+function calculateTicketPrice(works) {
+    let summ = 0;
+    for (const work of works) {
+        summ += WorksCollection.getPrice(work);
+    }
+    return summ;
+}
+
+// создаем информацию о заявке
+function createTicketInfo(ticket) {
+    const { name, id, content } = ticket;
+    const works = parseTicketWorks(content);
+    const price = calculateTicketPrice(works);
+    return { id, name, works, price };
+}
+
+function calculateTotalSumm(ticketsInfo) {
+    const totalSumm = ticketsInfo.reduce((accumulator, ticketInfo) => {
+        return accumulator + ticketInfo.price;
+    }, 0);
+    return totalSumm;
+}
